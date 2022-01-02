9c395e190aa788e3fde6573335ac6844b50fc698
0siribix <nomail@me.pl>
2022-01-02 00:35:55 -0600

    Added server priv check

--- init.lua
+++ init.lua
@@ -3,7 +3,8 @@ minetest.override_item("nether:lava_crust", {
 	on_place = function(stack, placer, pt)
 		p = minetest.get_pointed_thing_position(pt)
 		if p.y > 0 then
-			if placer and placer:get_player_name() ~= "" then
+			if placer and placer:get_player_name() ~= "" and
+					minetest.check_player_privs(placer:get_player_name(), "server") == false then
 				minetest.chat_send_player(placer:get_player_name(), "Unable to place lava crust above 0")
 				return
 			end
