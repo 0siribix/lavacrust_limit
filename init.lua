899df7735218a2972f1db10f0291eceb3e4e8119
0siribix <nomail@me.pl>
2022-01-02 00:21:15 -0600

    first commit

---
+++ init.lua
@@ -0,0 +1,13 @@
+
+minetest.override_item("nether:lava_crust", {
+	on_place = function(stack, placer, pt)
+		p = minetest.get_pointed_thing_position(pt)
+		if p.y > 0 then
+			if placer and placer:get_player_name() ~= "" then
+				minetest.chat_send_player(placer:get_player_name(), "Unable to place lava crust above 0")
+				return
+			end
+		end
+		minetest.item_place(stack, placer, pt)
+	end
+})
